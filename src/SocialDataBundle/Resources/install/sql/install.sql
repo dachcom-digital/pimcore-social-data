e8e8d7fa3fd47b0a7e6e24f5835635d811eb9940
Stefan Hagspiel <shagspiel@dachcom.ch>
2020-10-07 17:34:02 +0200

    update install script

--- src/SocialDataBundle/Resources/install/sql/install.sql
+++ src/SocialDataBundle/Resources/install/sql/install.sql
@@ -1,4 +1,3 @@
--- Create syntax for TABLE 'social_data_connector_engine'
 CREATE TABLE `social_data_connector_engine` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -6,9 +5,18 @@ CREATE TABLE `social_data_connector_engine` (
   `configuration` longtext COMMENT '(DC2Type:object)',
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQ_EA791E1F999517A` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
--- Create syntax for TABLE 'social_data_feed'
+CREATE TABLE `social_data_wall` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `data_storage` longtext COMMENT '(DC2Type:array)',
+  `asset_storage` longtext COMMENT '(DC2Type:array)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_9AEC7963999517A` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
 CREATE TABLE `social_data_feed` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `connector` int(11) DEFAULT NULL,
@@ -22,9 +30,8 @@ CREATE TABLE `social_data_feed` (
   KEY `IDX_AA59D23E13F5EFF6` (`wall`),
   CONSTRAINT `FK_AA59D23E13F5EFF6` FOREIGN KEY (`wall`) REFERENCES `social_data_wall` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_AA59D23E148C456E` FOREIGN KEY (`connector`) REFERENCES `social_data_connector_engine` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
--- Create syntax for TABLE 'social_data_feed_post'
 CREATE TABLE `social_data_feed_post` (
   `feed_id` int(11) NOT NULL,
   `post_id` int(11) NOT NULL,
@@ -33,7 +40,14 @@ CREATE TABLE `social_data_feed_post` (
   CONSTRAINT `FK_ADCA841C51A5BC03` FOREIGN KEY (`feed_id`) REFERENCES `social_data_feed` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Create syntax for TABLE 'social_data_feed_tags'
+CREATE TABLE `social_data_tag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_type` (`name`,`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
 CREATE TABLE `social_data_feed_tags` (
   `feed_id` int(11) NOT NULL,
   `tag_id` int(11) NOT NULL,
@@ -44,7 +58,16 @@ CREATE TABLE `social_data_feed_tags` (
   CONSTRAINT `FK_9750D294BAD26311` FOREIGN KEY (`tag_id`) REFERENCES `social_data_tag` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Create syntax for TABLE 'social_data_log'
+CREATE TABLE `social_data_wall_tags` (
+  `wall_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL,
+  PRIMARY KEY (`wall_id`,`tag_id`),
+  KEY `IDX_D16A4DE2BAD26311` (`tag_id`),
+  KEY `IDX_D16A4DE2C33923F1` (`wall_id`),
+  CONSTRAINT `FK_D16A4DE2BAD26311` FOREIGN KEY (`tag_id`) REFERENCES `social_data_tag` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_D16A4DE2C33923F1` FOREIGN KEY (`wall_id`) REFERENCES `social_data_wall` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `social_data_log` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(255) NOT NULL,
@@ -60,35 +83,4 @@ CREATE TABLE `social_data_log` (
   CONSTRAINT `FK_45821D5C13F5EFF6` FOREIGN KEY (`wall`) REFERENCES `social_data_wall` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_45821D5C234044AB` FOREIGN KEY (`feed`) REFERENCES `social_data_feed` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_45821D5C3EEACC32` FOREIGN KEY (`connector_engine`) REFERENCES `social_data_connector_engine` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Create syntax for TABLE 'social_data_tag'
-CREATE TABLE `social_data_tag` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name_type` (`name`,`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4;
-
--- Create syntax for TABLE 'social_data_wall'
-CREATE TABLE `social_data_wall` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `creation_date` datetime NOT NULL,
-  `data_storage` longtext COMMENT '(DC2Type:array)',
-  `asset_storage` longtext COMMENT '(DC2Type:array)',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UNIQ_9AEC7963999517A` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Create syntax for TABLE 'social_data_wall_tags'
-CREATE TABLE `social_data_wall_tags` (
-  `wall_id` int(11) NOT NULL,
-  `tag_id` int(11) NOT NULL,
-  PRIMARY KEY (`wall_id`,`tag_id`),
-  KEY `IDX_D16A4DE2BAD26311` (`tag_id`),
-  KEY `IDX_D16A4DE2C33923F1` (`wall_id`),
-  CONSTRAINT `FK_D16A4DE2BAD26311` FOREIGN KEY (`tag_id`) REFERENCES `social_data_tag` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `FK_D16A4DE2C33923F1` FOREIGN KEY (`wall_id`) REFERENCES `social_data_wall` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
